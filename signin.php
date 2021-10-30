8ae9ecaee134bc00894d00c2d9ab64c403aa99f0
Pranshu <71051334+Dash-7-2@users.noreply.github.com>
2021-10-29 21:35:58 -0400

    Add files via upload

---
+++ signin.php
@@ -0,0 +1,25 @@
+<?php
+$con = mysqli_connect('localhost','root','','facebookclone');
+
+$id = $_POST['id'];
+$firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$phoneoremail = $_POST['phoneoremail'];
+$password = $_POST['id'];
+
+$check_data = mysqli_query($con, "SELECT * FROM userdata WHERE phoneoremail = '$phoneoremail' and password = '$password'");
+$check = mysqli_nume_rows($check_data);
+
+if($check > 0){
+    echo "Email Already Exists";
+}
+else{
+    $imput = myspli_query($con, "INSERT INTO userdata(id,firstname,lastname,phoneoremail,pasword) Values ('$id','$firstname','$lastname','$phoneoremail','$password')")
+    if($input){
+        echo "Sign up succesful";
+    }
+    else{
+        echo "error occured";
+    }
+}
+?>
